c2e2c6e9c31c2601f65616abf2261c459ab27854
TAN SVEN <108319539+iamsven2005@users.noreply.github.com>
2025-09-26 14:52:16 +0800

    Update mdx.tsx

--- components/mdx.tsx
+++ components/mdx.tsx
@@ -3,6 +3,7 @@ import Image from 'next/image'
 import { MDXRemote } from 'next-mdx-remote/rsc'
 import { highlight } from 'sugar-high'
 import React from 'react'
+import remarkGfm from 'remark-gfm'
 
 function Table({ data }: any) {
   let headers = data.headers.map((header: any, index: any) => (
